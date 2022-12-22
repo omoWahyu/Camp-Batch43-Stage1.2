477275cad953798146c8943ce3f48498f45553f8
hyujisf <canqalloid.me@gmail.com>
2022-12-23 00:20:18 +0700

    fix some bug when showing data

--- assets/js/project.js
+++ assets/js/project.js
@@ -9,61 +9,64 @@ function getData(event) {
   let projectEnd = document.getElementById('project-end').value
   let projectDesc = document.getElementById('project-description').value
   let projectTech = document.getElementsByName('project-tech')
+  let projectImage = document.getElementById('project-image').files
 
   // let image = document.getElementById('f-project__image').value
   /* Cara diatas akan membuat file menjadi tidak terbaca dan mengubahnya sebagai fake path,
   karena file input akan menghasilkan data berupa object(URL.createObjectURL()), sehingga perlu diubah seperti berikut:
   */
 
-  image = URL.createObjectURL(image[0])
+  // ambil blob URL gambar
+  projectImage = URL.createObjectURL(projectImage[0])
+
+  let techChecked = []
+
+  for (let i = 0; i < projectTech.length; i++) {
+    if (projectTech[i].checked) {
+      techChecked.push(projectTech[i].value)
+    }
+  }
+
+  // console.log(projectName, projectStart, projectEnd, projectDesc, techChecked)
 
   let addProject = {
     projectName,
     projectStart,
     projectEnd,
     projectDesc,
+    projectImage,
+    techChecked
   }
 
-  let techCheck = []
+  dataProject.push(addProject)
+  showData() // agar show data tidak dijalankan berulang
 
-  for (let i = 0; i < projectTech.length; i++) {
-    if (projectTech[i].checked) {
-      techCheck.push(projectTech[i].value)
-    }
-  }
-
-  console.log(projectName, projectStart, projectEnd, projectDesc, techCheck)
 }
 
 function showData() {
-  document.getElementById("contents").innerHTML = ""
+  document.getElementById("list-content").innerHTML = ""
 
-  for (let i = 0; i <= blogs.length; i++) {
-    document.getElementById("contents").innerHTML += `
-      <div class="blog-list-item">
-      <div class="blog-image">
-        <img src="${blogs[i].image}" alt="" />
-      </div>
-      <div class="blog-content">
-        <div class="btn-group">
-          <button class="btn-edit">Edit Post</button>
-          <button class="btn-post">Post Blog</button>
-        </div>
-        <h1>
-          <a href="blog-detail.html" target="_blank"
-            >${blogs[i].title}</a
-          >
-        </h1>
-        <div class="detail-blog-content">
-          ${blogs[i].postedAt} | ${blogs[i].author}
-        </div>
-        <p>${blogs[i].content}
-        </p>
-        <div style="float:right; margin: 10px">
-          <p style="font-size: 15px; color:grey">1 minutes ago</p>
+  for (let i = 0; i <= dataProject.length; i++) {
+    document.getElementById("list-content").innerHTML += `
+    <div class="card" style="">
+        <img src="${dataProject[i].projectImage}" alt="">
+        <div class="card-body">
+          <div>
+            <h1>${dataProject[i].projectName}</h1>
+            <span>Durasi: 3Bulan</span>
+          </div>
+          <p>${dataProject[i].projectDesc}</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div>
+            <a href="#">Edit</a><a href="#">Delete</a>
+          </div>
         </div>
       </div>
-    </div>
       `
   }
 }
