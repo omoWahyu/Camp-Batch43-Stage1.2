78003204157dbe916fdd18b44ac809f3645415ac
hyujisf <canqalloid.me@gmail.com>
2022-12-23 17:16:53 +0700

    Update UI & finishing some feature

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -60,17 +60,54 @@ function getData(event) {
   */
   let image = document.getElementById('f-blog__image').files
 
-  // image = URL.createObjectURL(image[0])
-
-  // let addBlog = {
-  //   title,
-  //   content,
-  //   image
-  // }
-
-  // console.log(addBlog)
+  image = URL.createObjectURL(image[0])
 
   console.table(image)
-  // console.log(blogs)
 
+  let addBlog = {
+    title,
+    content,
+    image,
+    author: "Dandi Saputra",
+    postedAt: new Date()
+  }
+
+  blogs.push(addBlog)
+
+  console.log(blogs)
+  showData()
+
+}
+
+function showData() {
+  document.getElementById("contents").innerHTML = ""
+
+  for (let i = 0; i <= blogs.length; i++) {
+    document.getElementById("contents").innerHTML += `
+        <div class="blog-list-item">
+        <div class="blog-image">
+          <img src="${blogs[i].image}" alt="" />
+        </div>
+        <div class="blog-content">
+          <div class="btn-group">
+            <button class="btn-edit">Edit Post</button>
+            <button class="btn-post">Post Blog</button>
+          </div>
+          <h1>
+            <a href="blog-detail.html" target="_blank"
+              >${blogs[i].title}</a
+            >
+          </h1>
+          <div class="detail-blog-content">
+            ${blogs[i].postedAt} | ${blogs[i].author}
+          </div>
+          <p>${blogs[i].content}
+          </p>
+          <div style="float:right; margin: 10px">
+            <p style="font-size: 15px; color:grey">1 minutes ago</p>
+          </div>
+        </div>
+      </div>
+        `
+  }
 }
